a5013b763da96cc5b9b2075e7a8f5b48f22711ec
Pedro Vázquez <vanstrouble@proton.me>
2026-01-28 16:39:02 -0600

    Refactor promisesComponent to retrieve and render two heroes using promises

--- 03-advanced/src/concepts/03-promises.js
+++ 03-advanced/src/concepts/03-promises.js
@@ -11,14 +11,33 @@ export const promisesComponent = (element) => {
         `;
     };
 
+    const render2Hero = (hero1, hero2) => {
+        element.innerHTML += `
+            <h2>${hero1.name}</h2>
+            <img src="${hero1.picture}" alt="${hero1.name}" />
+            <p>${hero1.about}</p>
+            <h2>${hero2.name}</h2>
+            <img src="${hero2.picture}" alt="${hero2.name}" />
+            <p>${hero2.about}</p>
+        `;
+    };
+
     const renderError = (error) => {
         element.innerHTML = `<h3 style="color: red;">${error}</h3>`;
     };
 
     const id1 = '5d86371fd55e2e2a30fe1ccb2';
+    const id2 = '5d86371f25a058e5b1c8a65e';
 
     findHeroByID(id1)
-        .then( renderHero )
+        .then( (hero1) => {
+
+            findHeroByID(id2)
+                .then( hero2 => {
+                    render2Hero( hero1, hero2 );
+                })
+                .catch( renderError );
+        })
         .catch( renderError );
 };
 
