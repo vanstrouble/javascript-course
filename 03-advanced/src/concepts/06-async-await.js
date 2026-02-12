f59ca2edbe5e462958cede905281bf47e9e3c6de
Pedro Vázquez <vanstrouble@proton.me>
2026-02-12 17:12:31 -0600

    Refactor asyncAwaitComponent to include error handling for hero retrieval

--- 03-advanced/src/concepts/06-async-await.js
+++ 03-advanced/src/concepts/06-async-await.js
@@ -13,11 +13,15 @@ export const asyncAwaitComponent = async (element) => {
 		element.innerHTML = `<h2>${hero.name}</h2>`;
 	};
 
-	const hero1 = await findHero("5d86371f2343e37870b91ef1");
-	const hero2 = await findHero("5d86371f9f80b591f499df32");
-	const hero3 = await findHero("5d86371f233c9f2425f16916");
+    try {
+        const hero1 = await findHero("5d86371f2343e37870b91ef1");
+        const hero2 = await findHero("5d86371f9f80b591f499df32");
+        const hero3 = await findHero("5d86371f233c9f2425f16916");
 
-	element.innerHTML = `${hero1.name} / ${hero2.name} / ${hero3.name}`;
+        element.innerHTML = `${hero1.name} / ${hero2.name} / ${hero3.name}`;
+    } catch (error) {
+        element.innerHTML = `<p>${error.message}</p>`;
+    }
 };
 
 const findHero = async (id) => {
