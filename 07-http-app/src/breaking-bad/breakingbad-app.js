54d1cd0cfdbe9a31c16a2cbd79b2eb6cb6130179
Pedro Vázquez <vanstrouble@proton.me>
2026-02-18 23:35:03 -0600

    Refactor fetchQuote function and update BreakingBadApp to handle quote fetching and error display

--- 07-http-app/src/breaking-bad/breakingbad-app.js
+++ 07-http-app/src/breaking-bad/breakingbad-app.js
@@ -1,8 +1,39 @@
+const fetchQuote = async () => {
+    let id_rnd = Math.floor(Math.random() * 86) + 1;
+
+    const proxyUrl = "https://corsproxy.io/?";
+    const apiUrl = "https://api.mridul.tech/api/breaking-bad/quotes/" + id_rnd;
+
+    const response = await fetch(proxyUrl + encodeURIComponent(apiUrl));
+
+    const data = await response.json();
+    return data.data;
+}
+
 export const BreakingBadApp = (element) => {
-	console.log("BreakingBadApp called with element", element);
+	// console.log("BreakingBadApp called with element", element);
 
 	document.querySelector("#app-title").textContent = "Breaking Bad App";
 	element.innerHTML = `
         <p>Things are coming…</p>
     `;
+
+    fetchQuote().then(quote => {
+        console.log("Fetched quote", quote);
+        if (quote && quote.quote && quote.author) {
+            element.innerHTML = `
+                <p>${quote.quote}</p>
+                <p><em>- ${quote.author}</em></p>
+            `;
+        } else {
+            element.innerHTML = `
+                <p>No quote found. Please try again.</p>
+            `;
+        }
+    }).catch(error => {
+        console.error("Error fetching quote", error);
+        element.innerHTML = `
+            <p>Failed to fetch quote. Please try again later.</p>
+        `;
+    });
 };
