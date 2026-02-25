d777b3bd1f40ee915014306c55f885471278f2e8
Pedro Vázquez <vanstrouble@proton.me>
2026-02-25 15:32:33 -0600

    Remove limit parameter from loadUsersByPage URL for full user data retrieval

--- 08-crud/src/users/usecases/load-users-by-page.js
+++ 08-crud/src/users/usecases/load-users-by-page.js
@@ -3,7 +3,7 @@
  * @param {Number} page
  */
 export const loadUsersByPage = async (page = 1) => {
-    const url = `${import.meta.env.VITE_BASE_URL}/users?_page=${page}&_limit=10`;
+    const url = `${import.meta.env.VITE_BASE_URL}/users?_page=${page}`;
     const response = await fetch(url);
     const data = await response.json();
 
