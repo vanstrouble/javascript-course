428fa7abe260258559486644c37a8ac1c64a3fd7
Pedro Vázquez <vanstrouble@proton.me>
2026-02-26 16:15:02 -0600

    Refactor import paths in localhost-user.mapper.js and load-users-by-page.js for consistency

--- 08-crud/src/users/usecases/load-users-by-page.js
+++ 08-crud/src/users/usecases/load-users-by-page.js
@@ -1,11 +1,17 @@
+import { localhostUserToModel } from "../mappers/localhost-user.mapper.js";
+import { User } from "../models/user.js";
+
 /**
  *
  * @param {Number} page
+ * @return {Promise<User[]>}
  */
 export const loadUsersByPage = async (page = 1) => {
     const url = `${import.meta.env.VITE_BASE_URL}/users?_page=${page}`;
     const response = await fetch(url);
-    const data = await response.json();
+    const {data} = await response.json();
 
-    console.log('Loaded users', data);
+    const users = data.map(userLike => localhostUserToModel(userLike));
+
+    return users;
 }
