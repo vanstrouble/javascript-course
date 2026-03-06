810652df12116f4b8c18e5a7c6b7e4e6fe11b9c5
Pedro Vázquez <vanstrouble@proton.me>
2026-03-06 16:20:26 -0600

    Implement saveUser functionality to create new users via API

---
+++ 08-crud/src/users/usecases/save-user.js
@@ -0,0 +1,27 @@
+import { User } from "../models/user.js";
+
+export const saveUser = async (userLike) => {
+    const user = new User(userLike);
+
+    //TODO: Here we need a mapper
+    if (user.id) {
+        throw new Error('Updating users is not implemented yet.');
+    }
+
+    const updatedUser = await createUser(user);
+    return updatedUser;
+};
+
+const createUser = async (user) => {
+    const url = `${import.meta.env.VITE_BASE_URL}/users`;
+    const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    const newUser = await response.json();
+    console.log({ newUser });
+    return newUser;
+}
