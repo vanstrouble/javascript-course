5f612ea4283b759a185ffdccd8dab9ec437fabc6
Pedro Vázquez <vanstrouble@proton.me>
2026-03-12 14:21:40 -0600

    Remove debug logging from deleteUserById function

--- 08-crud/src/users/usecases/delete-user-by-id.js
+++ 08-crud/src/users/usecases/delete-user-by-id.js
@@ -5,20 +5,14 @@
  */
 export const deleteUserById = async (id) => {
     const url = `${import.meta.env.VITE_BASE_URL}/users/${id}`;
-    console.log('Attempting to delete user at URL:', url);
-    console.log('User ID:', id, 'Type:', typeof id);
 
     const response = await fetch(url, {
         method: 'DELETE',
     });
 
-    console.log('Delete response status:', response.status);
-    console.log('Delete response ok:', response.ok);
-
     if (!response.ok) {
         throw new Error(`Failed to delete user: ${response.status} ${response.statusText}`);
     }
 
-    console.log(`User ${id} deleted from server`);
     return true;
 }
